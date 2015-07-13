d08bef969bf1420220a5cc47719c5c0aac4714ea
Gethin Webster <gethin.webster@agilityworks.co.uk>
2015-07-13 15:35:15 +0100

    Update tests for render-fields

--- tests/integration/components/render-fields-test.js
+++ tests/integration/components/render-fields-test.js
@@ -1,27 +1,54 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import {moduleForComponent, test} from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 
 moduleForComponent('render-fields', 'Integration | Component | render fields', {
-  integration: true
+    integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(2);
+function createFields(...fields) {
+    return fields.map(field => Ember.Object.create(field));
+}
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it renders the title', function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{render-fields}}`);
+    this.set('fields', createFields({
+        title: 'Field One'
+    }));
+    this.render(hbs `{{render-fields fields=fields}}`);
 
-  assert.equal(this.$().text(), '');
+    assert.ok(this.$().html().match(/<dt>\s*Field One\s*<\/dt>/), 'should render title:' + this.$().html());
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#render-fields}}
-      template block text
-    {{/render-fields}}
-  `);
+test('it renders the value', function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+    this.set('fields', createFields({
+        field: 'field1'
+    }));
+    this.set('record', Ember.Object.create({
+        field1: 'foo'
+    }));
+    this.render(hbs `{{render-fields fields=fields record=record}}`);
+
+    assert.ok(this.$().html().match(/<dd>\s*foo\s*<\/dd>/), 'should render value:' + this.$().html());
+});
+
+test('it displays an input when `isEditing`', function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
+
+    this.set('fields', createFields({
+        field: 'field1'
+    }));
+    this.set('record', Ember.Object.create({
+        field1: 'foo'
+    }));
+    this.render(hbs `{{render-fields fields=fields record=record isEditing=true}}`);
+
+    assert.equal(this.$('input').length, 1, 'should render an input');
 });
